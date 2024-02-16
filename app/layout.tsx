8889ea59fc5a2177965df38c3dbfce304c3e97d7
Gor-Hoveyan <gorpro2004@mail.ru>
2024-02-17 02:46:08 +0400

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {
+  children: React.ReactNode,
+}) { 
   return (
     <html lang="en">
       <body className={inter.className}>
