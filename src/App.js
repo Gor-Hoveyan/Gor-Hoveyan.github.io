83048aed032774571a6c1fb228a7f0bcd5e953d5
Gor-Hoveyan <gorpro2004@mail.ru>
2024-08-01 12:32:04 +0400

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { Element } from 'react-scroll';
 import styles from './App.module.scss';
 import About from './components/about/About';
-import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Works from './components/works/Works';
 import Contact from './components/contact/Contact';
@@ -9,19 +7,10 @@ import Contact from './components/contact/Contact';
 function App() {
   return (
     <div className={styles.main}>
-      <Header />
-      <Element name='home'>
-        <Home />
-      </Element>
-      <Element name='about'>
-        <About />
-      </Element>
-      <Element name='works'>
-        <Works />
-      </Element>
-      <Element name='contact'>
-        <Contact />
-      </Element>
+      <Home />
+      <About />
+      <Works />
+      <Contact />
     </div>
   );
 }
