1792f001bd6fc94808687b0101feae243a0fdc14
Gor-Hoveyan <gorpro2004@mail.ru>
2024-09-14 07:03:33 +0400

    UI improvements

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,16 +7,14 @@ export default function About() {
       <h1 className={styles.header}>
         I'm looking forward to working with you.
       </h1>
-      <pre className={styles.content}>
+      <div className={styles.content}>
         I am a success-oriented web developer from Armenia. I enjoy creating
         efficient and user-friendly full-stack applications using{" "}
         <strong style={{ color: "#5BE9B9" }}>React/Next.js</strong> for
         front-end and <strong style={{ color: "#5BE9B9" }}>Node.js</strong> for
-        back-end.
-        <br />
-        I'm always up for a new challenge, excited to tackle complex problems
-        and continue expanding my skills in web development.
-      </pre>
+        back-end. I'm always up for a new challenge, excited to tackle complex
+        problems and continue expanding my skills in web development.
+      </div>
       <div className={styles.skills}>
         <h1 className={styles.header}>Skills</h1>
         <div className={styles.skillsSection}>
