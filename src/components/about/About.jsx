461efc6b8eff8ee998aad028a95300b3128d9633
Hoveyan Gor <gorpro2004@mail.ru>
2025-05-07 15:00:46 +0200

    Enhance skills section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,49 @@
 import React from "react";
 import styles from "./About.module.scss";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaSass,
+  FaNodeJs,
+  FaReact,
+  FaGit,
+  FaGitAlt,
+  FaGithub,
+  FaGitlab,
+  FaNpm,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiJavascript,
+  SiTypescript,
+  SiRedux,
+  SiNextdotjs,
+  SiExpress,
+  SiMysql,
+  SiMongodb,
+} from "react-icons/si";
+import { TbApi } from "react-icons/tb";
+
+const skills = [
+  { icon: <FaHtml5 size={30}/>, name: "HTML" },
+  { icon: <FaCss3Alt  size={30}/>, name: "CSS" },
+  { icon: <FaSass  size={30}/>, name: "SASS/SCSS" },
+  { icon: <SiTailwindcss  size={30}/>, name: "Tailwind" },
+  { icon: <SiJavascript  size={30}/>, name: "JavaScript" },
+  { icon: <SiTypescript  size={30}/>, name: "TypeScript" },
+  { icon: <FaReact  size={30}/>, name: "React" },
+  { icon: <SiRedux  size={30}/>, name: "Redux"},
+  { icon: <SiNextdotjs  size={30}/>, name: "Next.js" },
+  { icon: <FaNpm  size={30}/>, name: "npm" },
+  { icon: <FaNodeJs  size={30}/>, name: "Node.js" },
+  { icon: <SiExpress size={30}/>, name: "Express" },
+  { icon: <TbApi size={30} />, name: "REST API" },
+  { icon: <SiMongodb size={30} />, name: "MongoDB" },
+  { icon: <SiMysql size={30}/>, name: "MySQL" },
+  {icon: <FaGitAlt size={30}/>, name: "Git"},
+  {icon: <FaGithub size={30}/>, name: "GitHub"},
+  {icon: <FaGitlab size={30}/>, name: "GitLab"},
+];
 
 export default function About() {
   return (
@@ -17,23 +61,14 @@ export default function About() {
       </section>
       <section className={styles.skills}>
         <h1 className={styles.header}>Skills</h1>
-        <section className={styles.skillsSection}>
-          <span>HTML</span>
-          <span>CSS</span>
-          <span>SASS/SCSS</span>
-          <span>Tailwind</span>
-          <span>JavaScript</span>
-          <span>TypeScript</span>
-          <span>React</span>
-          <span>Redux</span>
-          <span>Zustand</span>
-          <span>Next.js</span>
-          <span>Node.js</span>
-          <span>Express</span>
-          <span>Rest API</span>
-          <span>MongoDB</span>
-          <span>MySQL</span>
-        </section>
+        <div className={styles.skillGrid}>
+          {skills.map(skill => (
+            <div key={skill.name} className={styles.skillItem}>
+              <div className={styles.icon}>{skill.icon}</div>
+              <div className={styles.skillTitle}>{skill.name}</div>
+            </div>
+          ))}
+          </div>
       </section>
     </section>
   );
