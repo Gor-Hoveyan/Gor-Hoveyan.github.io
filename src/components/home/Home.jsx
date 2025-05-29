02b6a142574ebe8f4ab81e98cdce1f77a6c3ff39
Hoveyan Gor <gorpro2004@mail.ru>
2025-05-29 13:47:32 +0200

    Content fixes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,8 +10,8 @@ export default function Home({onContactClick}) {
       <section className={styles.contentDiv}>
         <h1 className={styles.homeHeader}>HEY, I'M GOR HOVEYAN.</h1>
         <p className={styles.content}>
-          A MERN stack focused software engineer specializing in building{" "}
-          <span>Websites</span> and <span>Web Applications</span> that leeds to
+          A Front-End focused software engineer specializing in building{" "}
+          <span>Websites</span> and <span>Web Applications</span> that leads to
           success of overall product.
         </p>
       </section>
