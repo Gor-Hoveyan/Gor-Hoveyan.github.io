9d1868d6f7edf31dfd7a8011f80e9da1cb241022
Gor-Hoveyan <gorpro2004@mail.ru>
2025-08-17 02:22:01 +0400

    Add curve svg's at the top and bottom of the works section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,93 +4,103 @@ import { ImLinkedin } from "react-icons/im";
 import { FaTelegram, FaGithub } from "react-icons/fa6";
 import { ImMail4 } from "react-icons/im";
 
-const Contact = forwardRef((props, ref) =>  {
+const Contact = forwardRef((props, ref) => {
   const date = new Date();
   return (
-    <footer className={styles.main} ref={ref}>
-      <p className={styles.content}>Live. Create. Explore.</p>
-      <section className={styles.contacts}>
-        <a
-          className={styles.link}
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/gorhoveyan/"
-        >
-          <ImLinkedin
-            onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
-            onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
-            style={{
-              transitionDuration: "0.4s",
-              boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.6)",
-              borderRadius: "100%",
-            }}
-            color="white"
-            size={"2.5rem"}
-          />
-        </a>
-        <a
-          className={styles.link}
-          target="_blank"
-          rel="noreferrer"
-          href="https://t.me/gorhoveyan"
-        >
-          <FaTelegram
-            onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
-            onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
-            style={{
-              transitionDuration: "0.4s",
-              boxShadow: "4px 4px 15px rgba(0, 0, 0, 0.6)",
-              borderRadius: "50%",
-            }}
-            color="white"
-            size={"2.5rem"}
-          />
-        </a>
-        <a
-          className={styles.link}
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/Gor-Hoveyan"
-        >
-          <FaGithub
-            onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
-            onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
-            style={{
-              transitionDuration: "0.4s",
-              boxShadow: "4px 4px 15px rgba(0, 0, 0, 0.6)",
-              borderRadius: "50%",
-            }}
-            color="white"
-            size={"2.5rem"}
-          />
-        </a>
-        <a
-          className={styles.link}
-          target="_blank"
-          rel="noreferrer"
-          href="mailto:gorhoveyan@bk.ru"
-        >
-          <ImMail4
-            onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
-            onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
-            style={{
-              transitionDuration: "0.4s",
-              boxShadow: "4px 4px 15px rgba(0, 0, 0, 0.6)",
-              borderRadius: "50%",
-            }}
-            color="white"
-            size={"2.5rem"}
-          />
-        </a>
-      </section>
-      <div className={styles.footer}>
-        <p className={styles.credentials}>
-          Handcrafted by Gor Hoveyan <br />{" "}
-          <span className={styles.date}>© 2022-{date.getFullYear()}</span>
-        </p>
-      </div>
-    </footer>
+    <>
+      <svg
+        className={styles.svgCurve}
+        viewBox="0 0 1440 56"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#6e07f3"
+      >
+        <path d="M-100 0C-100 0 218.416 55.835 693.5 55.835C1168.58 55.835 1487 0 1487 0V79H-100V0Z"></path>
+      </svg>
+      <footer className={styles.main} ref={ref}>
+        <p className={styles.content}>Live. Create. Explore.</p>
+        <section className={styles.contacts}>
+          <a
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/gorhoveyan/"
+          >
+            <ImLinkedin
+              onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
+              onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
+              style={{
+                transitionDuration: "0.4s",
+                boxShadow: "4px 4px 10px rgba(0, 0, 0, 0.6)",
+                borderRadius: "100%",
+              }}
+              color="white"
+              size={"2.5rem"}
+            />
+          </a>
+          <a
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://t.me/gorhoveyan"
+          >
+            <FaTelegram
+              onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
+              onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
+              style={{
+                transitionDuration: "0.4s",
+                boxShadow: "4px 4px 15px rgba(0, 0, 0, 0.6)",
+                borderRadius: "50%",
+              }}
+              color="white"
+              size={"2.5rem"}
+            />
+          </a>
+          <a
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Gor-Hoveyan"
+          >
+            <FaGithub
+              onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
+              onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
+              style={{
+                transitionDuration: "0.4s",
+                boxShadow: "4px 4px 15px rgba(0, 0, 0, 0.6)",
+                borderRadius: "50%",
+              }}
+              color="white"
+              size={"2.5rem"}
+            />
+          </a>
+          <a
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+            href="mailto:gorhoveyan@bk.ru"
+          >
+            <ImMail4
+              onMouseOver={(e) => (e.currentTarget.style.scale = "1.2")}
+              onMouseLeave={(e) => (e.currentTarget.style.scale = "1")}
+              style={{
+                transitionDuration: "0.4s",
+                boxShadow: "4px 4px 15px rgba(0, 0, 0, 0.6)",
+                borderRadius: "50%",
+              }}
+              color="white"
+              size={"2.5rem"}
+            />
+          </a>
+        </section>
+        <div className={styles.footer}>
+          <p className={styles.credentials}>
+            Handcrafted by Gor Hoveyan <br />{" "}
+            <span className={styles.date}>© 2022-{date.getFullYear()}</span>
+          </p>
+        </div>
+      </footer>
+    </>
   );
-})
+});
 
 export default Contact;
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,5 @@
-import React, { forwardRef } from 'react'
-import styles from './Skills.module.scss';
+import React, { forwardRef } from "react";
+import styles from "./Skills.module.scss";
 import {
   FaHtml5,
   FaCss3Alt,
@@ -24,40 +24,50 @@ import {
 import { TbApi } from "react-icons/tb";
 
 const skills = [
-  { icon: <FaHtml5 size={30}/>, name: "HTML" },
-  { icon: <FaCss3Alt  size={30}/>, name: "CSS" },
-  { icon: <FaSass  size={30}/>, name: "SASS/SCSS" },
-  { icon: <SiTailwindcss  size={30}/>, name: "Tailwind" },
-  { icon: <SiJavascript  size={30}/>, name: "JavaScript" },
-  { icon: <SiTypescript  size={30}/>, name: "TypeScript" },
-  { icon: <FaReact  size={30}/>, name: "React" },
-  { icon: <SiRedux  size={30}/>, name: "Redux"},
-  { icon: <SiNextdotjs  size={30}/>, name: "Next.js" },
-  { icon: <FaNpm  size={30}/>, name: "npm" },
-  { icon: <FaNodeJs  size={30}/>, name: "Node.js" },
-  { icon: <SiExpress size={30}/>, name: "Express" },
+  { icon: <FaHtml5 size={30} />, name: "HTML" },
+  { icon: <FaCss3Alt size={30} />, name: "CSS" },
+  { icon: <FaSass size={30} />, name: "SASS/SCSS" },
+  { icon: <SiTailwindcss size={30} />, name: "Tailwind" },
+  { icon: <SiJavascript size={30} />, name: "JavaScript" },
+  { icon: <SiTypescript size={30} />, name: "TypeScript" },
+  { icon: <FaReact size={30} />, name: "React" },
+  { icon: <SiRedux size={30} />, name: "Redux" },
+  { icon: <SiNextdotjs size={30} />, name: "Next.js" },
+  { icon: <FaNpm size={30} />, name: "npm" },
+  { icon: <FaNodeJs size={30} />, name: "Node.js" },
+  { icon: <SiExpress size={30} />, name: "Express" },
   { icon: <TbApi size={30} />, name: "REST API" },
   { icon: <SiMongodb size={30} />, name: "MongoDB" },
-  { icon: <SiMysql size={30}/>, name: "MySQL" },
-  {icon: <FaGitAlt size={30}/>, name: "Git"},
-  {icon: <FaGithub size={30}/>, name: "GitHub"},
-  {icon: <FaGitlab size={30}/>, name: "GitLab"},
+  { icon: <SiMysql size={30} />, name: "MySQL" },
+  { icon: <FaGitAlt size={30} />, name: "Git" },
+  { icon: <FaGithub size={30} />, name: "GitHub" },
+  { icon: <FaGitlab size={30} />, name: "GitLab" },
 ];
 
-const Skills = forwardRef((props, ref) =>  {  
-    return (
+const Skills = forwardRef((props, ref) => {
+  return (
+    <>
       <section className={styles.skills} ref={ref}>
         <h1 className={styles.header}>Skills</h1>
         <div className={styles.skillGrid}>
-          {skills.map(skill => (
+          {skills.map((skill) => (
             <div key={skill.name} className={styles.skillItem}>
               <div className={styles.icon}>{skill.icon}</div>
               <div className={styles.skillTitle}>{skill.name}</div>
             </div>
           ))}
-          </div>
-      </section>  
-      )
-})
+        </div>
+      </section>
+      <svg
+        className={styles.svgCurve}
+        viewBox="0 0 1440 55"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#6e07f3"
+      >
+        <path d="M-100 0C-100 0 218.416 55.835 693.5 55.835C1168.58 55.835 1487 0 1487 0V56H-100V0Z"></path>
+      </svg>
+    </>
+  );
+});
 
 export default Skills;
